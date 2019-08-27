d866631488f269b17fcd8c9f0813fb9442e9798f
CarnunMP <carnun@hotmail.co.uk>
2019-08-27 21:38:29 +0100

    JQuery (I think that's what it is) on nav elements working!

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
-function toggleHomepage() {
-    let x = document.getElementsByClassName("container homepage");
-    if (x.style.display === "none") {
-        x.style.display = "initial";
-    } else {
-        x.style.display = "none";
-    }
+const homePage = document.getElementsByClassName("container homepage");
+const jokeWallet = document.getElementsByClassName("container joke-wallet");
+const loginSignup = document.getElementsByClassName("container login-signup");
+const pages = [homePage, jokeWallet, loginSignup];
+
+function switchTo(pageName) {
+    pages.forEach(page => page[0].style.display = "none");
+
+    let toPage = document.getElementsByClassName(pageName);
+    toPage[0].style.display = "inherit";
 }
