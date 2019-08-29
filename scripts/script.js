62b6026434871cf66ef9226dd3d9974437567649
CarnunMP <carnun@hotmail.co.uk>
2019-08-29 10:47:35 +0100

    attempt made at logging inputs to login-signup fields...

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,23 @@ function switchTo(pageName) {
         document.getElementsByTagName("footer")[0].style.display = "inherit";
     }
 }
+
+// To log login/signup submissions to the console:
+// function print(event) {
+//     event.preventDefault();
+//     let loginUsername = document.getElementById("#l-username");
+//     let loginPassword = document.getElementById("#l-password");
+//     let signupEmail = document.getElementById("#s-email");
+//     let signupUsername = document.getElementById("#s-username");
+//     let signupPassword1 = document.getElementById("#s-password1");
+//     let signupPassword2 = document.getElementById("#s-password2");
+//     console.log(`Login Username: ${loginUsername}`);
+//     console.log(`Login Password: ${loginPassword}`);
+//     console.log(`signup Email: ${signupEmail}`);
+//     console.log(`signup Username: ${signupUsername}`);
+//     console.log(`signup Password1: ${signupPassword1}`);
+//     console.log(`signup Password2: ${signupPassword2}`);
+// }
+
+// ^Got stuck with this one! It kept printing nulls...
+// Commented out and deleted the 'onsubmit's from index.html...
